3def87c21a265c3abb103c15701c05c6294fd20c
Deokjae <deokjj@gmail.com>
2017-08-22 03:20:57 -0400

    -

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,6 @@ export const environment = {
   production: false,
   apiBase: 'http://localhost:3000',
   googleKey: 'AIzaSyAUz5OS7yIcPzYX0nHN3RC7FSbexR2iH3U',
-  AWS_ACCESS_KEY_ID:'AKIAJ6GNPO4LELNQM2CA',
-  AWS_SECRET_ACCESS_KEY:'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'
+  AWS_ACCESS_KEY_ID:'AKIAJ4TQPOFSTWRGULFA',
+  AWS_SECRET_ACCESS_KEY:'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'
 };
