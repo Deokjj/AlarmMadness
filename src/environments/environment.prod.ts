0e1fb93f5cb76782c574d24b48fc6fffce1df964
Deokjae <deokjj@gmail.com>
2017-08-22 03:34:00 -0400

    y

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiBase: '',
-  googleKey: 'AIzaSyAUz5OS7yIcPzYX0nHN3RC7FSbexR2iH3U',
-  AWS_ACCESS_KEY_ID:'AKIAJWIHDY4MPCAPX3UQ',
-  AWS_SECRET_ACCESS_KEY:'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'
-};
